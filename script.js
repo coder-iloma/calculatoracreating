8d51caa7cee0d7995b161cc9ef0f81a2dab7510f
coder-iloma <aayushamoli.123@outlook.com>
2019-11-30 19:54:26 +0530

    Added these files for the first time.

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(){
+		var input1=parseInt(document.getElementById('input1').value);
+		var input2=parseInt(document.getElementById('input2').value);
+		var outputfield=input1+input2;
+		document.getElementById('outputfield').innerHTML=outputfield;
+	}
+function sub(){
+		var input1= document.getElementById('input1').value;
+		var input2= document.getElementById('input2').value;
+		var outputfield=input1-input2;
+		document.getElementById('outputfield').innerHTML=outputfield;
+	}
+function multi(){
+		var input1= document.getElementById('input1').value;
+		var input2= document.getElementById('input2').value;
+		var outputfield=input1*input2;
+		document.getElementById('outputfield').innerHTML=outputfield;
+	}
+function div(){
+		var input1= document.getElementById('input1').value;
+		var input2= document.getElementById('input2').value;
+		var outputfield=input1/input2;
+		document.getElementById('outputfield').innerHTML=outputfield;
+	}
+function clr(){
+		var input1= document.getElementById('input1').value="";
+		var input2= document.getElementById('input2').value="";
+	}
